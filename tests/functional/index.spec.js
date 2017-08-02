5f0231b85ba79eafa8a9a8acd7905bdbc7b65190
oupirum <oupirum@gmail.com>
2017-08-02 08:13:44 +0300

    Update func test

--- tests/functional/index.spec.js
+++ tests/functional/index.spec.js
@@ -9,6 +9,12 @@ describe('client;', ()=> {
 	
 	beforeEach((done)=> {
 		server = spawn('node', ['./bin/index.js']);
+		server.stdout.on('data', (data)=> {
+			console.log('server:', data.toString());
+		});
+		server.stderr.on('data', (data)=> {
+			console.log('server:', data.toString());
+		});
 		
 		setTimeout(()=> {
 			sockets = [];
@@ -34,6 +40,7 @@ describe('client;', ()=> {
 		
 		setTimeout(()=> {
 			expect(messages[0][0].event).toBe('room_created');
+			expect(messages[0][0].payload.id).toBe('1');
 			expect(messages[0][0].payload.name).toBe('room 1');
 			expect(messages[0][0].room_id).toBe('1');
 			
@@ -79,6 +86,33 @@ describe('client;', ()=> {
 		}, 30);
 	});
 	
+	it('reconnect;', (done)=> {
+		createRoom(sockets[0], 'room 1');
+		connect(sockets[1], '1');
+		connect(sockets[2], '1');
+		
+		setTimeout(()=> {
+			createRoom(sockets[1], 'room 2');
+			
+			setTimeout(()=> {
+				expect(messages[0].length).toBe(4);
+				expect(messages[0][3].event).toBe('client_left');
+				
+				expect(messages[1].length).toBe(4);
+				expect(messages[1][3].event).toBe('room_created');
+				expect(messages[1][3].payload.id).toBe('2');
+				expect(messages[1][3].room_id).toBe('2');
+				
+				expect(messages[2].length).toBe(4);
+				expect(messages[2][3].event).toBe('client_left');
+				
+				expect(messages[0][3].sender_id).toBe(messages[1][3].sender_id);
+				expect(messages[1][3].sender_id).toBe(messages[2][3].sender_id);
+				
+				done();
+			}, 30)
+		}, 30);
+	});
 	
 	function createRoom(socket, name) {
 		socket.sendMessage({
